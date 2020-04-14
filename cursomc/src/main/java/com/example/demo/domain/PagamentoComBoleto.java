5eaa0aa84dfa178a85220d597a09b51d525fcad9
fábio <fabiouds@hotmail.com>
2020-04-14 11:32:21 +0100

    pedido, pagamento e estado pagamento

---
+++ cursomc/src/main/java/com/example/demo/domain/PagamentoComBoleto.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.example.demo.domain.enums.EstadoPagamento;
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private Date dataVencimento;
+	private Date dataPagamento;
+	public PagamentoComBoleto() {
+		
+	}
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento=dataPagamento;
+		this.dataVencimento=dataVencimento;
+		// TODO Auto-generated constructor stub
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+}
